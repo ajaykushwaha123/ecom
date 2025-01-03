abef450d1e0405d82698febcfc813ed3c18ee203
AJAY KUSHWAHA <ajayindia734@gmail.com>
2025-01-03 20:07:47 +0530

    update

--- products/urls.py
+++ products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from products.views import get_product
 urlpatterns = [
     
-    path('<slug>/', get_product, name="index")
+    path('<slug>/', get_product, name="get_product")
     
     
 ]
